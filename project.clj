6b3d45b8d951f9e7c225c6fdb130626e8872dc0f
Andrew Monks <a@monks.co>
2015-12-17 02:20:16 -0600

    devcards

--- project.clj
+++ project.clj
@@ -1,7 +1,41 @@
-(defproject om-tutorial "0.1.0-SNAPSHOT"
+(defproject clovis "0.1.0-SNAPSHOT"
   :description "Clovis"
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [org.clojure/clojurescript "1.7.170"]
                  [org.omcljs/om "1.0.0-alpha22"]
                  [devcards "0.2.1-2"]
-                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
+                 #_[figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]]
+
+  :plugins [[lein-cljsbuild "1.1.1"]
+            [lein-figwheel "0.5.0-1"]]
+
+  :clean-targets ^{:protect false} ["resources/public/js/compiled"
+                                    "target"]
+
+  :source-paths ["src"]
+
+  :cljsbuild {
+              :builds [{:id "devcards"
+                        :source-paths ["src"]
+                        :figwheel { :devcards true } ;; <- note this
+                        :compiler { :main       "clovis.core"
+                                    :asset-path "js/compiled/devcards_out"
+                                    :output-to  "resources/public/js/compiled/clovis_devcards.js"
+                                    :output-dir "resources/public/js/compiled/devcards_out"
+                                    :source-map-timestamp true }}
+                       {:id "dev"
+                        :source-paths ["src"]
+                        :figwheel true
+                        :compiler {:main       "clovis.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/clovis.js"
+                                   :output-dir "resources/public/js/compiled/out"
+                                   :source-map-timestamp true }}
+                       {:id "prod"
+                        :source-paths ["src"]
+                        :compiler {:main       "clovis.core"
+                                   :asset-path "js/compiled/out"
+                                   :output-to  "resources/public/js/compiled/clovis.js"
+                                   :optimizations :advanced}}]}
+
+  :figwheel { :css-dirs ["resources/public/css"] })
--- src/clovis/core.cljs
+++ src/clovis/core.cljs
@@ -1,8 +1,18 @@
 (ns clovis.core
   (:require [goog.dom :as gdom]
             [om.next :as om :refer-macros [defui]]
-            [om.dom :as dom]))
+            [om.dom :as dom])
+  (:require-macros
+    [devcards.core :refer [defcard]]))
 
 (enable-console-print!)
 
 (println "Hello world!")
+
+(defcard "hello world")
+
+(defn main []
+  (if-let [node (.getElementById js/document "main-app-area")]
+    (println "launch app")))
+
+(main)
