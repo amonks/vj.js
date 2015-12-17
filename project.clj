89681d8fb14a46ff4b269bff9aa9e1b51f3534ae
Andrew Monks <a@monks.co>
2015-12-17 00:43:38 -0600

    init

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject om-tutorial "0.1.0-SNAPSHOT"
+  :description "Clovis"
+  :dependencies [[org.clojure/clojure "1.7.0"]
+                 [org.clojure/clojurescript "1.7.170"]
+                 [org.omcljs/om "1.0.0-alpha22"]
+                 [devcards "0.2.1-2"]
+                 [figwheel-sidecar "0.5.0-SNAPSHOT" :scope "test"]])
---
+++ src/clovis/core.cljs
@@ -0,0 +1,8 @@
+(ns clovis.core
+  (:require [goog.dom :as gdom]
+            [om.next :as om :refer-macros [defui]]
+            [om.dom :as dom]))
+
+(enable-console-print!)
+
+(println "Hello world!")
